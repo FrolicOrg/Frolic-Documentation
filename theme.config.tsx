e0d514386155728f8da697e1c3029dc9194f93a1
Arihant Parsoya <parsoyaarihant@gmail.com>
2023-08-06 23:25:29 +0530

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -24,6 +24,17 @@ const config: DocsThemeConfig = {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Frolic" />
       <meta property="og:description" content="Open Source backend service to create dashboards" />
+
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=G-RBE25D37DW`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RBE25D37DW');`
+        }}>
+      </script>
     </>
   )
 }
